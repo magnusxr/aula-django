dd2b3594a134dfd6a973b0c3465c5441f2ffd992
Magnus Xavier <magnusxavierr@outlook.com>
2025-01-15 21:01:40 -0300

    adições

--- projeto/loja_informatica/views.py
+++ projeto/loja_informatica/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Produto
 
 # Create your views here.
+
+def lista_produtos(request):
+    produtos = Produto.objects.all()  # busca todos os produtos
+    return render(request, 'loja_informatica/lista_produtos.html', {'produtos': produtos})
